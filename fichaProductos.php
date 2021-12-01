d19d2d0675988c31b8e9d9f558e4d849d69ab491
aitorfi <aitorfidalgo@gmail.com>
2021-12-01 20:45:05 +0100

    Tabla de productos implementada

--- fichaProductos.php
+++ fichaProductos.php
@@ -3,6 +3,17 @@
 
     $bd = new BaseDatos();
     $conexion = $bd->conectar();
+
+    function eliminarProducto($idProducto) {
+        global $bd;
+        $bd->eliminar("delete from producto where idProducto = $idProducto");
+    }
+
+    if (isset($_POST['eliminar'])) {
+        // Usar subStr para quitar la barra que aparece
+        // al final de la id del producto.
+        eliminarProducto(subStr($_POST['eliminar'], 0, -1));
+    }
 ?>
 
 <html>
@@ -25,15 +36,30 @@
         <div class="bodyProd">
             <table>
                 <tr>
-                    <th></th>
+                    <th>Nombre</th>
+                    <th>Tipo</th>
+                    <th>Unidad</th>
+                    <th>Descripci&oacute;n</th>
+                    <th>PVP</th>
+                    <th>Descuento</th>
+                    <th>Eliminar</th>
                 </tr>
                 <?php
                     $resProd = $bd->seleccionar("select * from producto");
                     while ($prod = $resProd->fetch_assoc()) {
                 ?>
-                    <tr>
-                        <td></td>
-                    </tr>
+                    <form method="POST" action=<?php echo $_SERVER['PHP_SELF'];?>>
+                        <tr>
+                            <td><?php echo $prod['ProductoNombre'];?></td>
+                            <td><?php echo $prod['idTipoProducto'];?></td>
+                            <td><?php echo $prod['Unidad'];?></td>
+                            <td><?php echo $prod['Descripcion'];?></td>
+                            <td><?php echo $prod['pvpUnidad'];?></td>
+                            <td><?php echo $prod['Descuento'];?></td>
+                            <td><input type="submit"/></td>
+                            <input type="hidden" name="eliminar" value=<?php echo $prod['idProducto']?>/>
+                        </tr>
+                    </form>
                 <?php }?>
             </table>
         </div>
